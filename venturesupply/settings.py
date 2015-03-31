08ad0b08d87cbe8fd38f4bf0bd39b4b3738916db
John Kang <johnkang.h@gmail.com>
2015-03-30 22:15:18 -0700

    Setup postgres db

--- venturesupply/settings.py
+++ venturesupply/settings.py
@@ -58,8 +58,12 @@ WSGI_APPLICATION = 'venturesupply.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'venturesupply_db',                  
+        'USER': 'venturesupply',
+        'PASSWORD': 'company53detail',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
