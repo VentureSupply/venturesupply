c1bd3e1116340bde9d180ae4f0b553b758d6c296
John Kang <johnkang.h@gmail.com>
2015-04-06 00:29:14 -0700

    script.py scrapes data from tables for a search for corporation GOOG.

--- search/script.py
+++ search/script.py
@@ -5,4 +5,5 @@ f = open("curl.txt", "r")
 curl = f.read()
 shell_output = subprocess.check_output(curl, shell=True)
 soup = BeautifulSoup(shell_output)
-print(soup.find_all('td'))
+for td in soup.find_all('td'):
+    print(td.text)
