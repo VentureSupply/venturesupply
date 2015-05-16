8d638a7031c84e71f638f44ce8b52f371ffcb0ff
John Kang <johnkang.h@gmail.com>
2015-05-15 22:28:27 -0700

    Test name search

--- search/models.py
+++ search/models.py
@@ -2,9 +2,8 @@ from django.db import models
 
 # Create your models here.
 
-class CorporationNameSearch(models.Model):
-    entity_number = models.CharField(max_length=50)
-    date_filed = models.DateTimeField('date filed')
+class BusinessData(models.Model):
+    name = models.CharField(max_length=50)
+    certificate = models.CharField(max_length=15)
+    file_date = models.DateTimeField('date filed')
     status = models.CharField(max_length=20)
-    entity_name = models.CharField(max_length=20)
-    agent_for_service = models.CharField(max_length=20)
--- search/script.py
+++ search/script.py
@@ -1,9 +1,29 @@
 from bs4 import BeautifulSoup
 import subprocess
 
-f = open("curl.txt", "r")
-curl = f.read()
-shell_output = subprocess.check_output(curl, shell=True)
-soup = BeautifulSoup(shell_output)
-for td in soup.find_all('td'):
-    print(td.text)
+
+def curl():
+    f = open("curl.txt", "r")
+    curl = f.read()
+    shell_output = subprocess.check_output(curl, shell=True)
+    soup = BeautifulSoup(shell_output)
+    scrape(soup)
+
+def scrape(soup):
+    table = soup.find_all('table')
+    trs = table[2].findAll("tr")[4:]
+    business_list = []
+    business_data = {}
+    for tr in trs:
+        tds = tr.find_all('td')
+        name = str.strip(tds[0].text)
+        status = str.strip(tds[3].text)
+        business_data = {
+            'name': name,
+            'status': status,
+        }
+        business_list.append(business_data)
+        for b in business_list:
+            print(b)
+
+curl()
